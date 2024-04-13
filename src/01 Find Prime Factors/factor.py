cd145ecbd926425d9bbd20b6b081381c4c878402
galomari <75580564+galomari@users.noreply.github.com>
2024-04-13 18:46:31 +0000

    new_approach

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -1,17 +1,18 @@
-def get_prime_factor(number):
-    factors=[]
-    divisor=2
-    while divisor <= number:
-        if number % divisor == 0:
-           factors.append(divisor)
-           number= number // divisor
-
-        else:
-             divisor+=1
+def get_prime_factors(number):
+    factors = []
+    # Loop over each number from 2 up to 99
+    for i in range(2, 100):
+        while number % i == 0:  # While 'i' is a factor of 'number'
+            factors.append(i)  # Add 'i' to the list of factors
+            number //= i       # Divide 'number' by 'i'
+        if number == 1:  # If 'number' has been fully factored
+            break
+    if number > 1:  # If 'number' is greater than 1 after exiting the loop, it is a prime factor.
+        factors.append(number)
     return factors
 
 
 # commands used in solution video for reference
 if __name__ == '__main__':
-    print(get_prime_factor(630))  # [2, 3, 3, 5, 7]
-    print(get_prime_factor(13))  # [13]
+    print(get_prime_factors(630))  # [2, 3, 3, 5, 7]
+    print(get_prime_factors(13))  # [13]
