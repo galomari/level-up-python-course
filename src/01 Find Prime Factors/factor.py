747561992100c2ad774a62b4cc95897854378fe8
galomari <75580564+galomari@users.noreply.github.com>
2024-04-13 18:03:45 +0000

    modified function name

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -1,4 +1,4 @@
-def get_prime_factors(number):
+def get_prime_factor(number):
     factors = []
     divisor = 2
     while divisor <= number:
@@ -12,5 +12,5 @@ def get_prime_factors(number):
 
 # commands used in solution video for reference
 if __name__ == '__main__':
-    print(get_prime_factors(630))  # [2, 3, 3, 5, 7]
-    print(get_prime_factors(13))  # [13]
+    print(get_prime_factor(630))  # [2, 3, 3, 5, 7]
+    print(get_prime_factor(13))  # [13]
