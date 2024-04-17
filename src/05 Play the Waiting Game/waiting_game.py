f3076af49ba031132269c3d44ffa724e9fc6a71d
galomari <75580564+galomari@users.noreply.github.com>
2024-04-17 15:11:40 +0000

    practice_new

--- src/05 Play the Waiting Game/waiting_game.py
+++ src/05 Play the Waiting Game/waiting_game.py
@@ -1,23 +1,19 @@
-import time
 import random
-
+import time
 def waiting_game():
-    target = random.randint(2, 4)  # target seconds to wait
-    print(f'\nYour target time is {target} seconds')
-
-    input(' ---Press Enter to Begin--- ')
-    start = time.perf_counter()
-
-    input(f'\n...Press Enter again after {target} seconds...')
-    elapsed = time.perf_counter() - start
-
-    print(f'\nElapsed time: {elapsed :.3f} seconds')
+    target=random.randit(2,4)
+    print(f"please wait {target} time and then press enter ")
+    input("please presse enter to begin")
+    timer=time.pref_counter()
+    input(f"please press enter again after{target} seconds")
+    elapsed = time.perf_counter() - timer
+     
     if elapsed == target:
-        print('(Unbelievable! Perfect timing!)')
-    elif elapsed < target:
-        print(f'({target - elapsed :.3f} seconds too fast)')
-    else:
-        print(f'({elapsed - target :.3f} seconds too slow)')
+         print(f"Elapsed time:{elapsed} seconds perfect timing!!")
+    elif elapsed > target:
+        print(f"Elapsed time:{target - elapsed :.3f} seconds too fast")
+    elif elapsed< target:
+        print(f"Elapsed time:{elapsed-target  :.3f} seconds too slow")
 
 
 # commands used in solution video for reference
