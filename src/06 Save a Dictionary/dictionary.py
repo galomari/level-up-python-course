090e3c89ad7c1d4c980e3ac4191b102b10ed69eb
galomari <75580564+galomari@users.noreply.github.com>
2024-04-17 15:49:50 +0000

    different_approach

--- src/06 Save a Dictionary/dictionary.py
+++ src/06 Save a Dictionary/dictionary.py
@@ -1,12 +1,12 @@
-import pickle
+import json
 
 def save_dict(dict_to_save, file_path):
-    with open(file_path, 'wb') as file:
-        pickle.dump(dict_to_save, file)
+    with open(file_path, 'w') as file:
+        json.dump(dict_to_save, file)
 
 def load_dict(file_path):
-    with open(file_path, 'rb') as file:
-        return pickle.load(file)
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
 
 # commands used in solution video for reference
